d80c4133f46deb84d298dbfd2eabdaa79848d3a2
karim mohamed <kmm200798@gmail.com>
2020-05-28 10:23:58 +0200

    first commit

--- src/app/components/frist-page/frist-page.component.ts
+++ src/app/components/frist-page/frist-page.component.ts
@@ -3,20 +3,14 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-frist-page',
   templateUrl: './frist-page.component.html',
-  styleUrls: ['./frist-page.component.css'],
- encapsulation: ViewEncapsulation.Native
+  styleUrls: ['./frist-page.component.css']
 })
 export class FristPageComponent implements OnInit {
-  public images;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.images = [
-      '../../../assets/img/r1.png',
-      '../../../assets/img/r3.png',
-      '../../../assets/img/r4.png'
-    ];
+    
   }
 
 }
